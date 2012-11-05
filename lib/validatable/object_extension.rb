dbd0a4fcf38dc80733c46009ad795c3919d5633b
Leif Gustafson <leif@aktechnicalconsultants.com>
2012-11-05 11:33:34 -0900

    Fix Thread behavior for 1.8/1.9 compatibility

--- lib/validatable/object_extension.rb
+++ lib/validatable/object_extension.rb
@@ -3,19 +3,38 @@ class Object #:nodoc:
   end
   include InstanceExecHelper
   def instance_eval_with_params(*args, &block)
-    begin
-      old_critical, Thread.critical = Thread.critical, true
-      n = 0
-      n += 1 while respond_to?(mname="__instance_exec#{n}")
-      InstanceExecHelper.module_eval{ define_method(mname, &block) }
-    ensure
-      Thread.critical = old_critical
+    # Compatibility check (Ruby 1.9 uses Thread.exclusive whereas Ruby 1.8 uses
+    # Thread.critical
+    if Thread.respond_to?(:critical=)
+      # The following is for Ruby 1.8 compatibility.  TODO: cleanup
+      begin
+        old_critical, Thread.critical = Thread.critical, true
+        n = 0
+        n += 1 while respond_to?(mname="__instance_exec#{n}")
+        InstanceExecHelper.module_eval{ define_method(mname, &block) }
+      ensure
+        Thread.critical = old_critical
+      end
+      begin
+        ret = send(mname, *args)
+      ensure
+        InstanceExecHelper.module_eval{ remove_method(mname) } rescue nil
+      end
+    else
+      # The following is for Ruby 1.9 compatibility.  TODO: cleanup
+      omname = Thread.exclusive do
+        n = 0
+        n += 1 while respond_to?(mname="__instance_exec#{n}")
+        InstanceExecHelper.module_eval{ define_method(mname, &block) }
+        mname
+      end
+      begin
+        ret = send(omname, *args)
+      ensure
+        InstanceExecHelper.module_eval{ remove_method(omname) } rescue nil
+      end
     end
-    begin
-      ret = send(mname, *args)
-    ensure
-      InstanceExecHelper.module_eval{ remove_method(mname) } rescue nil
-    end
+
     ret
   end
 end
